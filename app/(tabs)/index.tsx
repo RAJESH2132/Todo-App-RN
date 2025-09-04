700ad7c8ebd3dd1037c59897b86e8f5ac5f29094
RAJESH2132 <rajeshnagurla@gmail.com>
2025-09-04 22:23:52 +0530

    move components folder out of tabs folder

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,7 +3,7 @@ import useTheme from "@/hooks/useTheme";
 import { LinearGradient } from "expo-linear-gradient";
 import { StatusBar, Text, TouchableOpacity } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import Header from "./components/Header";
+import Header from "../../components/Header";
 
 export default function Index() {
   const { toggleDarkMode, colors } = useTheme();
